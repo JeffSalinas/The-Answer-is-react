377665ab7c2caf17a721153b3293847e2c52070c
Hack Reactor Student <student@hackreactor.com>
2019-08-01 15:00:38 -0500

    Create Jedi branch

--- client/components/App.js
+++ client/components/App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
   render() {
     return (
       <div id={'app'}>
-        What is Reactor 2?
+        What is Reactor
         {/* Gameboard */}
         {/* Scoreboard */}
         {/* Response */}
